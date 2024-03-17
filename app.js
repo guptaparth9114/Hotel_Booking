f939ba94981ddefc13b8d76e5d0e02d557bb70c4
guptaparth9114 <guptaparth9114@gmail.com>
2024-03-17 11:55:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public"))); // USED TO RENDER STATIC FILES LIKE CSS
 
 async function main() {
   await mongoose.connect(MONGO_URL);
